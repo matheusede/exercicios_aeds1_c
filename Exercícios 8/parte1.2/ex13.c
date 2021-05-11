5727ba1efe6125e79892e1209716ca28f844b55c
Matheus Marcolino <matheuseduardoleitemarcolino@gmail.com>
2021-05-10 23:00:17 -0300

    v 2.5
    Tudo pronto (menos a 25)

--- Exercícios 8/parte1.2/ex13.c
+++ Exercícios 8/parte1.2/ex13.c
@@ -27,13 +27,46 @@ void lerMatriz(int n1, int n2, int m[n1] [n2])
 void multiplicaLinhaMaiorNumero(int n1, int n2, int m [n1] [n2])
 {
     int maiorNumero;
-    for ( int j = 0; j < n2; j++)
+    for ( int i = 0; i < n1; i++)
     {
-        if (j > j -1)
+        for ( int j = 0; j < n2; j++)
         {
-            maiorNumero = j;
+            if (m [i] [j] > m [i - 1] [j - 1])
+            {
+                maiorNumero = m [i] [j];
+            }
         }
         
+        for ( int j = 0; j < n2; j++)
+        {
+            m [i] [j] = m [i] [j] * maiorNumero;
+        }
+
+        maiorNumero = 0;
     }
-    printf("%d", maiorNumero);
+}
+
+int main(void)
+{
+    //Declaração de variáveis.
+    int m [6] [4];
+
+    //leitura da matriz.
+    lerMatriz(6, 4, m);
+
+    //Cálculo
+    multiplicaLinhaMaiorNumero(6, 4, m);
+
+    //Output de dados.
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 4; j ++)
+        {
+            printf("%d ", m [i] [j]);
+        }
+        
+        printf("/n");
+    }
+    
+    return 0;
 }
